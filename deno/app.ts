03b168308b9918728437cfed029c3a17c93a85fc
Kenny Jinhiro <jxnhiro@gmail.com>
2023-11-03 01:04:57 +0700

    Deno CRUD Finished

--- deno/app.ts
+++ deno/app.ts
@@ -8,7 +8,10 @@ const app = new Application();
 
 app.use(async (ctx, next) => {
   ctx.response.headers.set("Access-Control-Allow-Origin", "*");
-  ctx.response.headers.set("Access-Control-Allow-Methods", "*");
+  ctx.response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE",
+  );
   ctx.response.headers.set("Access-Control-Allow-Headers", "Content-Type");
   await next();
 });
--- deno/routes/todos.ts
+++ deno/routes/todos.ts
@@ -1,14 +1,12 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { getDb } from "../helpers/db_client.ts";
-
+import { Bson } from "https://deno.land/x/mongo@v0.29.2/mod.ts";
 const router = new Router();
 
 interface Todo {
   id?: string;
   text: string;
 }
-
-let todos: Todo[] = [];
 
 router.get("/todos", async (ctx) => {
   const todos = await getDb().collection("todos").find().toArray();
@@ -31,18 +29,28 @@ router.post("/todos", async (ctx) => {
 });
 
 router.put("/todos/:todoId", async (ctx) => {
-  const tid = ctx.params.todoId;
+  const tid = ctx.params.todoId!;
   const { text } = await ctx.request.body().value;
-  const todoIndex = todos.findIndex((todo) => {
-    return todo.id === tid;
-  });
-  todos[todoIndex] = { id: todos[todoIndex].id, text: text };
+
+  await getDb()
+    .collection("todos")
+    .updateOne(
+      { _id: new Bson.ObjectId(tid) },
+      {
+        $set: { text: text },
+      },
+    );
   ctx.response.body = { message: "Updated todo" };
 });
 
-router.delete("/todos/:todoId", (ctx) => {
-  const tid = ctx.params.todoId;
-  todos = todos.filter((todo) => todo.id !== tid);
+router.delete("/todos/:todoId", async (ctx) => {
+  console.log(ctx.params);
+
+  const tid = ctx.params.todoId!;
+  console.log("Params", tid);
+  await getDb()
+    .collection("todos")
+    .deleteOne({ _id: new Bson.ObjectId(tid) });
   ctx.response.body = { message: "Deleted todo" };
 });
 
