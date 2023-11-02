c6d437f76e14ebd2431e3252146b82b279378660
Kenny Jinhiro <jxnhiro@gmail.com>
2023-11-02 23:01:20 +0700

    Added React and Local Backend

---
+++ deno/app.ts
@@ -0,0 +1,17 @@
+import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+
+import todosRoutes from "./routes/todos.ts";
+
+const app = new Application();
+
+app.use(async (ctx, next) => {
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+  ctx.response.headers.set("Access-Control-Allow-Methods", "*");
+  ctx.response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  await next();
+});
+
+app.use(todosRoutes.routes());
+app.use(todosRoutes.allowedMethods());
+
+await app.listen({ port: 8000 });
---
+++ deno/helpers/db.ts
@@ -0,0 +1,3 @@
+import { MongoClient } from "https://deno.land/x/mongo@0.32.0/mod.ts";
+
+const client = new MongoClient();
